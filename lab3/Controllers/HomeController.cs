80917e54c27252e271b7eeee14b76d67946414a8
hadeel <hadeelalsahikhsaleh@gmail.com>
2022-10-18 23:18:50 -0400

    Add project files.

---
+++ lab3/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            HttpContext.Session.SetString("numBottles", Request.Form["numBottles"]);
+
+            return View();
+        }
+
+        public IActionResult CreatePerson() => View();
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            if (person.FirstName == null)
+            {
+                return View("~/Views/Home/Error.cshtml");
+            }
+            // you will complete this
+            return View(person);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ lab3/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [BindProperty(Name = "first")]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
